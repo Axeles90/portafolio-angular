d6c7675768c0b37d367cfdebb0539f54cc495e99
Axel pollini <pablopolliniyepes@gmail.com>
2025-06-27 00:07:55 +0200

    creamos rutas

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+
+
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { Portafolio } from './pages/portafolio/portafolio'; 
+import { About } from './pages/about/about';
+import { Item } from './pages/item/item';
+
+const routes: Routes = [
+  { path: 'home', component: Portafolio },
+  {path: 'about', component: About},
+  {path: 'item' , component: Item},
+  {path: '**', redirectTo: 'home', pathMatch: 'full' } // Redirect to home for any unknown paths
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], 
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
