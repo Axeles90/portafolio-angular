db52c980ac451c1b0434ba39bb2646a53d7962cd
Axel pollini <pablopolliniyepes@gmail.com>
2025-06-26 19:23:41 +0200

    creamos repositorio github

---
+++ src/app/app-module.ts
@@ -0,0 +1,28 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { Portafolio } from './pages/portafolio/portafolio';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Footer,
+    Portafolio
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/pages/portafolio/portafolio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio',
+  standalone: false,
+  templateUrl: './portafolio.html',
+  styleUrl: './portafolio.css'
+})
+export class Portafolio {
+
+}
